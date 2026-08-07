57bcd933db02416e1ecf0dd7ee0e55a68515ee24
gabrielpc4 <4140799+gabrielpc4@users.noreply.github.com>
2026-08-07 20:40:35 -0300

    Use a stronger driver preview line

--- src/CharacterPreviewLineCatalog.cs
+++ src/CharacterPreviewLineCatalog.cs
@@ -31,7 +31,7 @@ internal sealed class CharacterPreviewLineCatalog
         ["Chair"] = "What are you looking at? Can't you see I'm exhausted? I can't move my legs.",
         ["David"] = "Where do you live, girl?",
         ["Robert"] = "What are you trying to achieve?",
-        ["Driver"] = "Who are you?",
+        ["Driver"] = "Girl? Can you hear me? Help me remember what I'm doing here!",
         ["Librarian"] = "Wonderful! Then I can introduce myself too. My name is Cruella Castellier; you can just call me Cruella."
     };
 
